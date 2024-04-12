a7273b4c52b5c502e7da42694e9c46271df82bd2
ramilep <rapar9510@oslomet.no>
2024-04-12 13:05:46 +0200

    oblig2 2024

---
+++ src/main/java/data1700oblig2/Data1700ObligApplication.java
@@ -0,0 +1,13 @@
+package data1700oblig2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Data1700ObligApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Data1700ObligApplication.class, args);
+    }
+
+}
---
+++ src/test/java/data1700oblig2/Data1700ObligApplicationTests.java
@@ -0,0 +1,13 @@
+package data1700oblig2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Data1700ObligApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
